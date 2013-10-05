0ab528ed05638ab059b058c58f5f7ff21277bae6
Jonathan Dizdarevic <dizzda@gmail.com>
2013-10-05 21:59:56 +0200

    install capifony

---
+++ app/config/deploy.rb
@@ -0,0 +1,37 @@
+set :application, "immoCrawlr"
+set :domain,      "root@bank.dizda.fr"
+set :deploy_to,   "/home/www/bank.dizda.fr"
+set :app_path,    "app"
+set :user,        "www-data"
+set :port,        7777
+
+set :scm,         :git
+set :repository,  "git@github.com:dizda/bank-manager.git"
+set :deploy_via,  :copy
+
+
+set :model_manager, "doctrine"
+
+role :web,        domain                         # Your HTTP server, Apache/etc
+role :app,        domain                         # This may be the same as your `Web` server
+role :db,         domain, :primary => true       # This is where Symfony2 migrations will run
+
+set  :use_sudo,      true
+set  :keep_releases, 3
+
+
+## Symfony2
+set :shared_files,        ["app/config/parameters.yml"]
+set :shared_children,     [app_path + "/logs", "vendor"]
+set :update_vendors,      false
+set :use_composer,        true
+#set :composer_options,    "--verbose --prefer-dist"     #install script
+set :writable_dirs,       ["app/cache", "app/logs"]
+set :webserver_user,      "www-data"
+set :permission_method,   :chown
+set :use_set_permissions, true
+set :dump_assetic_assets, true
+
+
+# Be more verbose by uncommenting the following line
+logger.level = Logger::MAX_LEVEL
